b9c385883090ac696472e5aa5694a92e5ad4f531
jopwork <jopwork@gmail.com>
2018-05-10 22:35:28 +0800

    Swift format specifications parser

--- src/test/java/RegexTest.java
+++ src/test/java/RegexTest.java
@@ -1,16 +1,28 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexTest {
+    private static final Pattern pattern = Pattern.compile(
+            "(\\d+(!?|\\*\\d+)[naxyzced])|//|[,:\\[\\]/]|(?<![a-zA-Z])N|[a-zA-Z]+");
 
     @Test
-    public void regexParse() {
-        Pattern pattern = Pattern.compile("(\\d+(!?|\\*\\d+)[naxyzced])|//|[,:\\[\\]/]|(?<![a-zA-Z])N|[a-zA-Z]+");
-        Matcher matcher = pattern.matcher(":4!c//8!n6!n[,3n][/[N]2!n[2!n]]\n");
-        while(matcher.find()){
-            System.out.println(matcher.group());
+    public void regexParse() throws Exception {
+        List<String> list = parseSwiftField(":4!c//8!n6!n[,3n][/[N]2!n[2!n]]");
+        for(String s : list){
+            System.out.println(s);
         }
+    }
+
+    private List<String> parseSwiftField(String fieldText) throws Exception {
+        List<String> result = new ArrayList<>();
+        Matcher matcher = pattern.matcher(fieldText);
+        while(matcher.find()){
+            result.add(matcher.group());
+        }
+        return result;
     }
 }
